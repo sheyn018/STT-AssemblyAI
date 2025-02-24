b9e140418ae0750ae14e186de94d62221e0a80b6
sheyn018 <sheane39@gmail.com>
2025-02-25 02:46:20 +0800

    finished draft for realtime transcription

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, Response
 from flask_sock import Sock
+import json
+import base64
+from twilio_transcriber import TwilioTranscriber
 
 PORT = 5000
 DEBUG = False
@@ -15,8 +18,11 @@ def receive_call():
         xml = f"""
 <Response>
     <Say>
-        You have connected to the Flask application
+        Speak to see your speech transcribed in the console
     </Say>
+    <Connect>
+        <Stream url='wss://{request.host}{WEBSOCKET_ROUTE}' />
+    </Connect>
 </Response>
 """.strip()
         return Response(xml, mimetype='text/xml')
@@ -25,8 +31,24 @@ def receive_call():
 
 @sock.route(WEBSOCKET_ROUTE)
 def transcription_websocket(ws):
-    pass
-
+    print('called')
+    while True:
+        data = json.loads(ws.receive())
+        match data['event']:
+            case "connected":
+                transcriber = TwilioTranscriber()
+                transcriber.connect()
+                print('transcriber connected')
+            case "start":
+                print('twilio started')
+            case "media":
+                payload_b64 = data['media']['payload']
+                payload_mulaw = base64.b64decode(payload_b64)
+                transcriber.stream(payload_mulaw)
+            case "stop":
+                print('twilio stopped')
+                transcriber.close()
+                print('transcriber closed')
 
 if __name__ == "__main__":
     app.run(port=PORT, debug=DEBUG)
